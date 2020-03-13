cae1d794100cc468520c53ba88c44f0969e7f437
Robert Jackowski <jackowski_robert@wp.pl>
2020-03-13 14:45:32 +0000

    Step37

--- webApp/Component.js
+++ webApp/Component.js
@@ -1,9 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"./controller/HelloDialog"
+	"./controller/HelloDialog",
+	"sap/ui/Device"
 
-], function (UIComponent, JSONModel, HelloDialog) {
+], function (UIComponent, JSONModel, HelloDialog, Device) {
 	"use strict";
 	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
 		metadata : {
@@ -20,11 +21,29 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
+			// disable batch grouping for v2 API of the northwind service
+			// this.getModel("invoice").setUseBatch(false)
+			
+			// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
 
 			// set dialog
 			this._helloDialog = new HelloDialog(this.getRootControl());
 		    // create the views based on the url/hash
 			this.getRouter().initialize();
+		},
+		
+		getContentDensityClass : function () {
+			if (!this._sContentDensityClass) {
+				if (!Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
 		},
 
 
